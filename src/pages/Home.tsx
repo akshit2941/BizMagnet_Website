b42db938c2e1967d1b9fa08a5cbf1186057eb275
akshit2941 <sainiakshit2941@gmail.com>
2024-05-15 13:56:57 +0530

    Added HeroSection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
-import heroImage from "../assets/image/hero-img-1.jpeg";
+import HeroSection from "../components/HomePage/HeroSection";
 
 const Home: React.FC = () => {
   return (
@@ -11,32 +11,8 @@ const Home: React.FC = () => {
       </div>
 
       {/* Hero Section */}
-      <div className="flex pl-5p pr-5p mt-28">
-        <div className="p-12">
-          {/* Content+Button */}
-          <div>
-            {/* Content */}
-            <h1 className="text-[#294A5E] text-7xl font-bold">
-              Elevate your business with precision and prowess
-            </h1>
-            <p className="text-[#294A5E] text-xl mt-8 tracking-wider" >
-              Through the application of advanced marketing techniques, we strive to revolutionize the way businesses approach their customers. Our team ensures that your business story resonates profoundly within your targeted audience.
-            </p>
-          </div>
-          <div className="mt-8 flex ">
-            <button className=" h-12 gap-x-1.5 text-white text-center cursor-pointer bg-[#457b9d] border border-[#457b9d] rounded-lg justify-center items-center text-base font-medium leading-none no-underline flex w-28">
-              Get Started
-            </button>
-            <button className=" text-lg font-medium ml-6 text-[#457b9d]">
-              Discover More→</button>
-          </div>
-        </div>
-
-        {/* Image */}
-        <div className="border rounded-xl p-6 static">
-          {/* img */}
-          <img src={heroImage} alt="HeroImage" className="rounded-lg " />
-        </div>
+      <div>
+        <HeroSection />
       </div>
     </div>
   );
